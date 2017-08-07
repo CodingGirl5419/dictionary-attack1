ae2f9b7f3a4c9a48325c6dcecb886876877ef045
CodingGirl5419 <alexandrab910@gmail.com>
2017-08-07 09:14:47 -0400

    Create dictionary-attack.js

---
+++ dictionary-attack.js
@@ -0,0 +1,25 @@
+var wordsList = [];
+
+function init() {
+  // Load the words from the dictionary text file to wordsList
+  var wordsFile = "https://raw.githubusercontent.com/GirlsFirst/SIP-2017/master/Unit2_Applications/dictionary-attack/dictionary.txt?token=ADcVhZjRMd86ZdhPE2jVvIaJdQdzLA6Yks5YvvVSwA%3D%3D";
+  $.get(wordsFile, function(data) {
+    document.getElementById("btnSubmit").disabled = true;
+    wordsList = data.split('\n');
+    document.getElementById("btnSubmit").disabled = false;
+  });
+}
+
+window.onload = init;
+
+/* ADD YOURoo CODE BELOW */
+
+function checkPassword() {
+  var user_input = document.getElementById("pw").value;
+  for (item in wordsList) {
+    if (wordsList[item] == user_input) {
+      alert("weak password");
+      break;
+    }
+  }
+}
